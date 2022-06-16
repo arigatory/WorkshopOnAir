356013f26a85f40fbd7b529752da9ff6b678bcd2
Ivan Panchenko <arigatory@gmail.com>
2022-06-16 12:47:01 +0300

    just started

---
+++ WorkshopOnAir/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace WorkshopOnAir;
+
+public static class Program
+{
+    public static async Task Main()
+    {
+        var httpClient = new HttpClient();
+
+        string data = await httpClient.GetStringAsync("http://msudotnet.ru/");
+        if (data.Length > 0)
+        {
+            Console.WriteLine(data.Substring(0,500));
+        }
+    }
+
+}
